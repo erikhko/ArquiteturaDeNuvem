7ee27ccdd55bcfb3002e6ffbc6272ad3643ee37a
Erik <erikhko@hotmail.com>
2024-09-11 22:36:49 -0300

    Aula de Proxy Reverso

---
+++ APP2/server.js
@@ -0,0 +1,13 @@
+//Criamos uma variável express
+const express = require('express');
+
+const app = express();
+const port = 3002;
+
+app.get('/', (req, res) => {
+    res.send('Resposta da API 2');
+});
+
+app.listen(port, () => {
+    console.log('API 2 escutando a porta ' + port);
+});
